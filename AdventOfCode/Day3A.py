3bd2e0afda3613698914ff70fea984f80d2052d1
Colm T <colm.kl.tang@gmail.com>
2022-12-03 18:01:42 +0000

    Day 3 complete

--- AdventOfCode/Day3A.py
+++ AdventOfCode/Day3A.py
@@ -1,1 +1,29 @@
+# Day 3 is about loading rucksacks with supplies, each rucksack has two large compartments,
+# The input is a text file with each line being a string corresponding to a rucksack, 
+# with the first half of the string being the first compartment and second half being the second compartment,
+# Each rucksack, has an item duplicated between the compartments, [rucksack = line, item = char, compartments = halves]
+# And each item has a priority value, a to z = 1 to 26 and A to Z = 27 to 52.
+# Find the duplicate in each rucksack and sum up their priorities
+
+def compare_compartments(first_compartment, second_compartment):
+    (duplicated_item, ) = first_compartment.intersection(second_compartment) # tuple to extract single value from set
+    item_priority = ord(duplicated_item)
+
+    if item_priority > 64 and item_priority < 91: # Capital letters
+        item_priority -= 38 # offset to make A = 27, etc.
+    elif item_priority > 96 and item_priority < 123:
+        item_priority -= 96 # offset to make a = 1
+    return item_priority
+
+
+with open("input/day3.txt", "r") as f:
+    summed_item_priorities = 0
+
+    for line in f.readlines():
+        sanitized_line = line.strip()
+        first_compartment = set(sanitized_line[:len(sanitized_line)//2])
+        second_compartment = set(sanitized_line[len(sanitized_line)//2:])
+        summed_item_priorities += compare_compartments(first_compartment, second_compartment)
+
+print(summed_item_priorities) # 8176
 
--- AdventOfCode/Day3B.py
+++ AdventOfCode/Day3B.py
@@ -1,1 +1,35 @@
+# Part B adds the issue that elves in squads of 3 have identical badges within their rucksacks,
+# Each set of 3 rucksacks in succession are in a squad. i.e. the first 3 rucksacks, the following 3 and so on,
+# Identify which item is shared within the 3 rucksacks to find which is the badge,
+# Go through each squad and sum up the priorities for each squad badge. i.e. only count each badge once per squad
 
+def get_badge_priority(badge):
+    (squad_badge, ) = badge # tuple to extract single value from set
+    squad_badge_priority = ord(squad_badge)
+
+    if squad_badge_priority > 64 and squad_badge_priority < 91: # Capital letters
+        squad_badge_priority -= 38 # offset to make A = 27, etc.
+    elif squad_badge_priority > 96 and squad_badge_priority < 123:
+        squad_badge_priority -= 96 # offset to make a = 1
+    return squad_badge_priority
+
+
+with open("input/day3.txt", "r") as f:
+    squad_count = 0
+    summed_badge_priorities = 0
+
+    for line in f.readlines():
+        squad_count += 1
+        sanitized_line = line.strip()
+        if squad_count == 1:
+            first_rucksack = set(sanitized_line)
+        elif squad_count == 2:
+            second_rucksack = set(sanitized_line)
+        elif squad_count == 3:
+            third_rucksack = set(sanitized_line)
+            squad_badge = set.intersection(first_rucksack, second_rucksack, third_rucksack)
+            squad_badge_priority = get_badge_priority(squad_badge)
+            summed_badge_priorities += squad_badge_priority
+            squad_count = 0
+
+print(summed_badge_priorities) # 2689
